bcb0141f070e420d636dfed2f8272e92c2348c5f
Aman Jain <aman.jain@if.se>
2024-12-13 14:28:30 +0100

    Added text label

--- bank/management/system/Login.java
+++ bank/management/system/Login.java
@@ -8,6 +8,7 @@ public class Login extends JFrame {
     Login() {
         setTitle("AUTOMATED TELLER MACHINE");
 
+        setLayout(null);
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("resources/icons/logo.jpg"));
         Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
 
@@ -16,7 +17,16 @@ public class Login extends JFrame {
 
         //To add an image on the frame, always add it via JLabel
         JLabel label = new JLabel(i3);
+
+        label.setBounds(70, 10, 100, 100);
         add(label);
+
+        JLabel text = new JLabel("Welcome to ATM");
+        text.setFont(new Font("Osward", Font.BOLD, 24));
+        text.setBounds(200, 40, 400, 40);
+        add(text);
+
+        getContentPane().setBackground(Color.WHITE);
 
         setSize(800, 480);
         // Only when setVisible is true, we can see the box
